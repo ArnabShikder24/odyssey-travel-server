8ea9a44f8ec544e92fc857ed4161b6598697a321
ArnabShikder24 <arnabshikder2001@gmail.com>
2024-09-16 23:56:02 +0600

    get all package api added

--- src/controllers/PackagesController.js
+++ src/controllers/PackagesController.js
@@ -19,3 +19,17 @@ exports.createPack = (req, res) => {
         res.status(201).json({ message: "Package created successfully"});
     });
 };
+
+exports.getAllPack = (req, res) => {
+  const sql = "SELECT * FROM packages";
+
+  db.query(sql, (err, results) => {
+      if (err) {
+          console.error("Error fetching packages:", err);
+          return res.status(500).json({ message: "Failed to fetch packages" });
+      }
+
+      console.log("packages fetched successfully");
+      res.status(200).json(results);
+  });
+};
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const ExampleController = require('../controllers/ExampleController');
 const { createUser, getAllUsers } = require('../controllers/userController');
+const { createPack, getAllPack } = require('../controllers/PackagesController');
 
 router.get("/example", ExampleController.example);
 
 
 router.post("/user/create", createUser);
 router.get("/users", getAllUsers);
+router.post("/pack/create", createPack);
+router.get("/pack/create", getAllPack);
 
 
 module.exports = router;
